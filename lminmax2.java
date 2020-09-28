cda214e79af9b65ae9e0965599360adbe6b1be76
Oralita <oat431@gmail.com>
2020-09-28 23:27:32 +0700

    write own lambda function on my own

---
+++ lminmax2.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+
+public class lminmax2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>(); // Test input 
+        arr.add(4);
+        arr.add(7);
+        arr.add(3);
+        arr.add(9);
+        arr.add(2);
+        arr.add(1);
+ 
+        CMP find = new CMP(); // CMP is just a class that contain lambda function 
+        System.out.println(find.MAX(arr));
+        System.out.println(find.MIN(arr));
+    }   
+}
+
+class CMP{
+    private Ops2 max; //declare Ops and rank
+    private Ops2 min; 
+    private Rank r; 
+
+    //setup lambda function
+    public CMP(){
+        
+        //setup min and max function
+        max = (a,b) -> {return a>b?a:b;};
+        min = (a,b) -> {return a<b?a:b;};
+
+        //setup iterator
+        r = (array,fun)->{
+            int temp = array.get(0);
+            for(int i : array){
+                temp = fun.cmp(temp, i);
+            }   
+            return temp;   
+        };
+    }
+
+    // declare traditional method
+    public int MIN(ArrayList<Integer> arr){
+        return r.rank(arr, min);
+    }
+
+    public int MAX(ArrayList<Integer> arr){
+        return r.rank(arr, max);
+    }
+
+}
+
+//setup Ops template
+interface Ops2{
+    int cmp(int a,int b);
+}
+
+//setup Rank template
+interface Rank{
+    int rank(ArrayList<Integer> arr,Ops2 fun);
+}
